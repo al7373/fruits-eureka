ce97947f38236ee728a2cbea6cc6f2331eb928a3
rra <rra@debian>
2023-04-08 19:09:41 +0300

    fields name to english

--- assets/vue/components/FruitList.vue
+++ assets/vue/components/FruitList.vue
@@ -1,29 +1,29 @@
 <!-- assets/vue/components/FruitList.vue -->
 <template>
   <div>
-    <h1>Liste des fruits</h1>
+    <h1>Fruits List</h1>
 
     <div>
-      <label for="name">Nom du fruit:</label>
+      <label for="name">Fruit name:</label>
       <input type="text" id="name" v-model="filters.name" @input="fetchFruits">
     </div>
 
     <div>
-      <label for="family">Famille:</label>
+      <label for="family">Family:</label>
       <input type="text" id="family" v-model="filters.family" @input="fetchFruits">
     </div>
 
     <ul>
       <li v-for="fruit in fruits" :key="fruit.id">
         {{ fruit.name }} - {{ fruit.family }}
-        <button @click="toggleFavorite(fruit)">Ajouter aux favoris</button>
+        <button @click="toggleFavorite(fruit)">Add to favorites</button>
       </li>
     </ul>
 
     <div>
-      <button @click="changePage(-1)" :disabled="currentPage === 1">Précédent</button>
+      <button @click="changePage(-1)" :disabled="currentPage === 1">Previous</button>
       Page {{ currentPage }}
-      <button @click="changePage(1)">Suivant</button>
+      <button @click="changePage(1)">Next</button>
     </div>
   </div>
 </template>
@@ -58,7 +58,7 @@ export default {
       this.fetchFruits();
     },
     async toggleFavorite(fruit) {
-      await axios.post(`http://localhost:3000/api/fruits/${fruit.id}/favorite`);
+      await axios.post(`http://localhost:3000/api/fruits/${fruit.fruit_id}/favorite`);
       fruit.isFavorite = !fruit.isFavorite;
     },
   },
